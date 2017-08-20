cc6980e7741e540839a1039ac9f59c01128f46a4
Natusya <natmihkr@gmail.com>
2017-08-21 01:14:08 +0200

    bootstrap connect

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,22 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var connect = require('gulp-connect');
+var path = require('path');
+var sassIncludePath = [
+  path.join(__dirname, '/node_modules/bootstrap/scss')
+];
 
-gulp.task('default', ['server', 'watch']);
+gulp.task('default', ['build', 'server', 'watch']);
+
+gulp.task('build', ['sass', 'html']);
 
 gulp.task('sass', function() {
   gulp.src('./src/styles/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass({
+      outputStyle: 'compressed',
+      includePaths: sassIncludePath
+    }).on('error', sass.logError))
     .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('build/css'))
     .pipe(connect.reload());
@@ -28,4 +37,5 @@ gulp.task('server', function () {
     livereload: true
   });
 });
+
 
