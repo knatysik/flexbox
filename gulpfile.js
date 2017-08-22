e497ccbdf7a77294767c306f9c0a1a3a584c5cfd
Natusya <natmihkr@gmail.com>
2017-08-23 00:32:24 +0200

    prepare to upload

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,16 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var connect = require('gulp-connect');
 var path = require('path');
+var imagemin = require('gulp-imagemin');
+var autoprefixer = require('gulp-autoprefixer');
+
 var sassIncludePath = [
   path.join(__dirname, '/node_modules/bootstrap/scss')
 ];
 
 gulp.task('default', ['build', 'server', 'watch']);
 
-gulp.task('build', ['sass', 'html']);
+gulp.task('build', ['sass', 'html', 'images']);
 
 gulp.task('sass', function() {
   gulp.src('./src/styles/*.scss')
@@ -18,6 +21,10 @@ gulp.task('sass', function() {
       outputStyle: 'compressed',
       includePaths: sassIncludePath
     }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('build/css'))
     .pipe(connect.reload());
@@ -37,5 +44,9 @@ gulp.task('server', function () {
     livereload: true
   });
 });
-
+gulp.task('images', function() {
+  gulp.src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('build/images'))
+});
 
